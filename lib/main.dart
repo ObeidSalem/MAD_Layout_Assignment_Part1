690ce6cbe71858b14d012fcbb62d22405f92c4c4
ObeidSalem <o.salem322@gmail.com>
2021-12-21 18:53:04 +0530

    Custom Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@
 // When you press the icon, toggle the image and toggle the text underneath between on and off.
 
 import 'package:flutter/material.dart';
+import 'device.dart';
+import 'device_container.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,12 +18,19 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  List<bool> device1 = [false];
-  List<bool> device2 = [false];
-  List<bool> device3 = [false];
-  List<bool> device4 = [false];
-  List<bool> device5 = [false];
-  List<bool> device6 = [false];
+  List<Device> devices = [
+    Device("Iron Machine", Icons.iron),
+    Device("Television", Icons.tv),
+    Device("Computer", Icons.computer),
+    Device("Desktop Computer", Icons.desktop_windows),
+    Device("PlayStation 5", Icons.sports_esports),
+    Device("Air Condition", Icons.ac_unit),
+
+  ];
+
+  Widget deviceTemplate(device){
+    return DeviceContainer(device);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,438 +40,20 @@ class _MyAppState extends State<MyApp> {
           title: Text("IoT App - Obeid"),
         ),
         body: Container(
-        color: Colors.grey[100] ,
-        padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+          color: Colors.grey[100] ,
+          padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
 
-        child: GridView.count(
-          childAspectRatio: (140 / 100),
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                   BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                 ],
-                ),
-                child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                    device1[0] ? Icon(
-                      Icons.iron,
-                      color: Colors.blue,
-                      size: 40,
-                    ):
-                    Icon(
-                      Icons.iron,
-                      color: Colors.grey,
-                      size: 40,
-                    ),
-                    ToggleButtons(
-                      children: const <Widget>[
-                        Icon(Icons.power_settings_new),
-                      ],
-                      isSelected: device1,
-                      color: Colors.grey,
-                      selectedColor: Colors.green,
-                      fillColor: Colors.transparent,
-                      borderRadius: BorderRadius.circular(50),
-                      selectedBorderColor: Colors.green,
-                      onPressed: (int index) {
-                        setState(() {
-                          device1[index] = !device1[index];
-                        });
-                      },
-                    ),
-                  ]),
-                  SizedBox(height: 10,),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          "Iron Machine",
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: device1[0] ? Text(
-                          "On",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.green),
-                        ): Text(
-                          "Off",
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                  ]),
-                ],
-              ),
-            ),
-
-
-
-
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        device2[0] ? Icon(
-                          Icons.tv,
-                          color: Colors.blue,
-                          size: 40,
-                        ):
-                        Icon(
-                          Icons.tv,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        ToggleButtons(
-                          children: const <Widget>[
-                            Icon(Icons.power_settings_new),
-                          ],
-                          isSelected: device2,
-                          color: Colors.grey,
-                          selectedColor: Colors.green,
-                          fillColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(50),
-                          selectedBorderColor: Colors.green,
-                          onPressed: (int index) {
-                            setState(() {
-                              device2[index] = !device2[index];
-                            });
-                          },
-                        ),
-                      ]),
-                  SizedBox(height: 10,),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Television",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: device2[0] ? Text(
-                            "On",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.green),
-                          ): Text(
-                            "Off",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ]),
-                ],
-              ),
-            ),
-
-
-
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        device3[0] ? Icon(
-                          Icons.computer,
-                          color: Colors.blue,
-                          size: 40,
-                        ):
-                        Icon(
-                          Icons.computer,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        ToggleButtons(
-                          children: const <Widget>[
-                            Icon(Icons.power_settings_new),
-                          ],
-                          isSelected: device3,
-                          color: Colors.grey,
-                          selectedColor: Colors.green,
-                          fillColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(50),
-                          selectedBorderColor: Colors.green,
-                          onPressed: (int index) {
-                            setState(() {
-                              device3[index] = !device3[index];
-                            });
-                          },
-                        ),
-                      ]),
-                  SizedBox(height: 10,),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Computer",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: device3[0] ? Text(
-                            "On",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.green),
-                          ): Text(
-                            "Off",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ]),
-                ],
-              ),
-            ),
-
-
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        device4[0] ? Icon(
-                          Icons.desktop_windows,
-                          color: Colors.blue,
-                          size: 40,
-                        ):
-                        Icon(
-                          Icons.desktop_windows,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        ToggleButtons(
-                          children: const <Widget>[
-                            Icon(Icons.power_settings_new),
-                          ],
-                          isSelected: device4,
-                          color: Colors.grey,
-                          selectedColor: Colors.green,
-                          fillColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(50),
-                          selectedBorderColor: Colors.green,
-                          onPressed: (int index) {
-                            setState(() {
-                              device4[index] = !device4[index];
-                            });
-                          },
-                        ),
-                      ]),
-                  SizedBox(height: 10,),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Desktop Computer",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: device4[0] ? Text(
-                            "On",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.green),
-                          ): Text(
-                            "Off",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ]),
-                ],
-              ),
-            ),
-
-
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        device5[0] ? Icon(
-                          Icons.sports_esports,
-                          color: Colors.blue,
-                          size: 40,
-                        ):
-                        Icon(
-                          Icons.sports_esports,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        ToggleButtons(
-                          children: const <Widget>[
-                            Icon(Icons.power_settings_new),
-                          ],
-                          isSelected: device5,
-                          color: Colors.grey,
-                          selectedColor: Colors.green,
-                          fillColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(50),
-                          selectedBorderColor: Colors.green,
-                          onPressed: (int index) {
-                            setState(() {
-                              device5[index] = !device5[index];
-                            });
-                          },
-                        ),
-                      ]),
-                  SizedBox(height: 10,),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Playstation 5",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: device5[0] ? Text(
-                            "On",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.green),
-                          ): Text(
-                            "Off",
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ]),
-                ],
-              ),
-            ),
-
-
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade200, spreadRadius: 2),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        device6[0] ? Icon(
-                          Icons.ac_unit,
-                          color: Colors.blue,
-                          size: 40,
-                        ):
-                        Icon(
-                          Icons.ac_unit,
-                          color: Colors.grey,
-                          size: 40,
-                        ),
-                        ToggleButtons(
-                          children: const <Widget>[
-                            Icon(Icons.power_settings_new),
-                          ],
-                          isSelected: device6,
-                          color: Colors.grey,
-                          selectedColor: Colors.green,
-                          fillColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(50),
-                          selectedBorderColor: Colors.green,
-                          onPressed: (int index) {
-                            setState(() {
-                              device6[index] = !device6[index];
-                            });
-                          },
-                        ),
-                      ]),
-                  SizedBox(height: 10,),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          "Air Condition",
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: device6[0] ? Text(
-                          "On",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.green),
-                        ): Text(
-                          "Off",
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                  ]),
-                ],
-              ),
-            ),
-          ]),
+          child: GridView.count(
+            childAspectRatio: (140 / 100),
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            children: devices.map((device) {
+              return DeviceContainer(device);
+            }).toList(),
           ),
         ),
+      ),
     );
   }
 }
